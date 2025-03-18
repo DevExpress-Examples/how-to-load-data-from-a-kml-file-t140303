060b20ece1bffd843bef8ba36e1788df1596b360
DevExpressExampleBot <fakeEmail>
2025-03-18 15:26:12 +0400

    Source auto update [skip ci]

--- VB/WinForms_MapControl_KmlFileDataAdapter/Form1.Designer.vb
+++ VB/WinForms_MapControl_KmlFileDataAdapter/Form1.Designer.vb
@@ -1,5 +1,7 @@
-﻿Namespace WinForms_MapControl_KmlFileDataAdapter
-    Partial Public Class Form1
+Namespace WinForms_MapControl_KmlFileDataAdapter
+
+    Partial Class Form1
+
         ''' <summary>
         ''' Required designer variable.
         ''' </summary>
@@ -10,14 +12,14 @@
         ''' </summary>
         ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
-        #Region "Windows Form Designer generated code"
-
+'#Region "Windows Form Designer generated code"
         ''' <summary>
         ''' Required method for Designer support - do not modify
         ''' the contents of this method with the code editor.
@@ -27,7 +29,7 @@
             Me.bingMapDataProvider1 = New DevExpress.XtraMap.BingMapDataProvider()
             Me.vectorItemsLayer1 = New DevExpress.XtraMap.VectorItemsLayer()
             Me.mapControl1 = New DevExpress.XtraMap.MapControl()
-            DirectCast(Me.mapControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.mapControl1), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
             ' 
             ' imageTilesLayer1
@@ -58,17 +60,17 @@
             Me.Controls.Add(Me.mapControl1)
             Me.Name = "Form1"
             Me.Text = "Form1"
-            DirectCast(Me.mapControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.mapControl1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
-
         End Sub
 
-        #End Region
-
+'#End Region
         Private mapControl1 As DevExpress.XtraMap.MapControl
+
         Private imageLayer1 As DevExpress.XtraMap.ImageLayer
+
         Private bingMapDataProvider1 As DevExpress.XtraMap.BingMapDataProvider
+
         Private vectorItemsLayer1 As DevExpress.XtraMap.VectorItemsLayer
     End Class
 End Namespace
-
--- VB/WinForms_MapControl_KmlFileDataAdapter/Form1.vb
+++ VB/WinForms_MapControl_KmlFileDataAdapter/Form1.vb
@@ -1,14 +1,15 @@
-﻿Imports DevExpress.XtraMap
+Imports DevExpress.XtraMap
 Imports System
 Imports System.Windows.Forms
 
 Namespace WinForms_MapControl_KmlFileDataAdapter
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits Form
 
-        Private Const filePath As String = "../../kmlFile.kml"
+        Const filePath As String = "../../kmlFile.kml"
 
-        Private ReadOnly Property KmlLayer() As VectorItemsLayer
+        Private ReadOnly Property KmlLayer As VectorItemsLayer
             Get
                 Return CType(mapControl1.Layers("KmlLayer"), VectorItemsLayer)
             End Get
@@ -16,12 +17,11 @@ Namespace WinForms_MapControl_KmlFileDataAdapter
 
         Public Sub New()
             InitializeComponent()
-
-'            #Region "#KmlFileDataAdapter"
+'#Region "#KmlFileDataAdapter"
             ' Create a KML file data adapter.
-            Dim baseUri As New Uri(System.Reflection.Assembly.GetEntryAssembly().Location)
+            Dim baseUri As Uri = New Uri(Reflection.Assembly.GetEntryAssembly().Location)
             KmlLayer.Data = New KmlFileDataAdapter() With {.FileUri = New Uri(baseUri, filePath)}
-'            #End Region ' #KmlFileDataAdapter
+'#End Region  ' #KmlFileDataAdapter
         End Sub
     End Class
 End Namespace
